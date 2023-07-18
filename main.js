91bdcef7b303fe9024ce71645480c168b961080d
duytran15032k@gmail.com <tranduy15932k@gmail.com>
2023-07-18 13:43:14 +0700

    home

---
+++ main.js
@@ -0,0 +1,69 @@
+// var parameterScroll= document.querySelector('.our-success__parameter--hidden');
+// window.onscroll = () =>{
+//     var topE = parameterScroll.offsetTop;
+//     var btmE = parameterScroll.offsetTop + parameterScroll.clientHeight;
+//     var topScreen= window.pageXOffset;
+//     var btmScreen = window.pageYOffset + window . innerHeight;
+//     if (btmScreen > topE && topScreen < btmE) {
+        
+//     }
+// }
+// function counter() {
+//     const counters = document.querySelectorAll('.our-success__parameter-value');
+// const speed = 200;
+// // const valueDataSet = document.querySelectorAll('[data-set]');
+// // valueDataSet.forEach( element => {
+// //     var getValue = element.getAttribute('data-set');
+// // });
+// counters.forEach( counter => {
+//     // const valueDataSet = +counter.getAttribute('data-set');
+  
+//    const animate = () => {
+//       const value = +counter.getAttribute('data-set');
+//       const data = +counter.innerText;
+//       const time = value / speed;
+//      if(data < speed) {
+//           counter.innerText = Math.ceil(data + time);
+//           setTimeout(animate, 0.1);
+//         }else{
+//           counter.innerText = value;
+//         }
+//    }
+//    animate();
+// });
+// }
+// $(document).ready(function(){
+//   $('.counter').counterUp({
+//     delay: 15,
+//     time: 1200
+//   })
+// });
+$(document).ready(function() {
+  var position = $('.our-success__parameter').offset().top;
+  var winHeight = $(window).height();
+  var count = 0;
+
+  $(window).scroll(function() {
+    var winTop = $(window).scrollTop();
+
+    // Xác định vị trí của phần tử có class "our-success__parameter"
+    if (position < winTop + winHeight && count == 0) {
+      $('.counter').each(function() {
+        var defaultValue = $(this).text();
+        $(this).prop('Counter', 0).animate({
+          Counter: defaultValue
+        }, {
+          duration: 1200,
+          easing: 'swing',
+          step: function(now) {
+            $(this).text(Math.ceil(now));
+          }
+        });
+      });
+      count = 1;
+    } else if (position > winTop + winHeight) {
+      count = 0; // Đặt lại biến count khi người dùng cuộn lên trên
+    }
+  });
+});
+
