ab4cf296ca0b3ce82c9ffb8a576c0699982fb931
duytran15032k@gmail.com <tranduy15932k@gmail.com>
2023-08-11 22:20:50 +0700

    done

--- main.js
+++ main.js
@@ -67,3 +67,24 @@ $(document).ready(function() {
   });
 });
 
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:true,
+  dots: false,
+  autoplay:true,
+  autoplayTimeOut:3000,
+  autoplaySpeed:1000,
+  navText: ['<i class="fa-solid fa-angle-left icon-angle icon-angle-left"></i>','<i class="fa-solid fa-angle-right icon-angle icon-angle-right"></i>'],
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:1
+      },
+      1000:{
+          items:1
+      }
+  }
+})
